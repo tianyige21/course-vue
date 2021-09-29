35f877b5fe091370731220dda8a6a41cd0b5e744
wangzhian <wangzhian@cmss.chinamobile.com>
2021-09-29 19:04:50 +0800

    feat:测试代码
    Code Source From: Self Code
    Description: add:
    Jira:
    市场项目编号（名称）:

---
+++ .history/vue-mdEditor/src/views/test_20210929142736.vue
@@ -0,0 +1,53 @@
+<!--  -->
+<template>
+<div>
+  1212
+</div>
+</template>
+
+<script>
+//import x from ''
+import { toRefs } from 'vue'
+ 
+
+export default {
+  name: 'Test',
+  setup(props) {
+	const { title } = toRefs(props)
+ 
+	console.log(title.value)
+},
+  props: {
+    
+},
+  data () {
+    return {
+    }
+  },
+  components: {
+    
+},
+  computed: {
+    
+},
+  watch: {
+    
+},
+  methods: {
+    
+},
+  beforeMount () { 
+    
+},
+  mounted () { 
+    
+},
+  activated () {
+    
+ }
+}
+</script>
+<style lang='less' scoped>
+/* @import url(); 引入css类 */
+
+</style>
